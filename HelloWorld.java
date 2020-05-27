8cb8000b1214c70a826b50025f064bcbcb35e740
Somasekhar <47001132+Somasekhargit@users.noreply.github.com>
2020-05-28 01:36:21 +0530

    made 3 change for web hook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
    public static void main(String[] args){
    
-   System.out.println("--------*******Hellow World and Welcome to Jenkins Training/made 2 change");
+   System.out.println("--------*******Hellow World and Welcome to Jenkins Training/made 3 change for web hook");
    }
  }
